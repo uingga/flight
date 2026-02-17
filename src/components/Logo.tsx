43ae1b0d67d30ce42a694718cbc781b086a02915
ynal <ynal@example.com>
2026-02-17 17:58:30 +0900

    로고: 심볼 위치 위로 조정

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,7 +11,7 @@ export default function Logo({ className = '', size = 1.0 }: { className?: strin
                 viewBox="0 0 24 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                style={{ flexShrink: 0, marginTop: `${-2 * scale}px` }}
+                style={{ flexShrink: 0, marginTop: `${-4 * scale}px` }}
             >
                 <path
                     d="M3.8 11.1 L20.2 2.9 Q22 2 21.2 3.8 L13.8 20.2 Q13 22 12.0 20.3 L9.5 15.7 Q8.5 14 6.6 13.4 L3.9 12.6 Q2 12 3.8 11.1Z"
