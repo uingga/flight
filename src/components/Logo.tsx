6f4c13b02234e60a02a06ee6da76737d89f575a5
ynal <ynal@example.com>
2026-02-19 14:14:00 +0900

    feat: 로고 폰트를 잘난체(YeogiOttaeJalnan)로 변경

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -26,7 +26,7 @@ export default function Logo({ className = '', size = 1.0 }: { className?: strin
                 </defs>
             </svg>
             <span style={{
-                fontFamily: "'Moneygraphy', sans-serif",
+                fontFamily: "'YeogiOttaeJalnan', sans-serif",
                 fontWeight: 'normal',
                 fontSize: `${2.0 * scale}rem`,
                 color: '#111827',
